96c769e734b90532049b85193af863d8bce659f8
Emanuele Barban <58031+McRipper@users.noreply.github.com>
2020-04-12 14:25:26 +0200

    Manage login in initializer

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -4,4 +4,22 @@ Trestle.configure do |config|
     render 'trestle/auth/otp'
   end
 
+  config.auth.authenticate = ->(params) {
+
+    scope = Trestle.config.auth.user_scope
+
+    user = scope.authenticate(params[Trestle.config.auth.authenticate_with], params[:password])
+
+    if user && user.otp_module?
+      if params[:otp_code_token].present? && user.authenticate_otp(params[:otp_code_token], drift: 60)
+        return user
+      else
+        return false
+      end
+    else
+      return user
+    end
+
+  }
+
 end
